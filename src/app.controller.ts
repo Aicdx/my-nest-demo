d62527f8b6d184555d9bcffab3fe528eb9f4d95a
acid <1261535644@qq.com>
2024-04-22 22:25:38 +0800

    aop切面编程

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TimeInterceptor } from './time.interceptor';
+import { ValidatePipe } from './validate.pipe';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('aaa')
+  aaa(): string {
+    console.log('aaa...');
+    return 'aaa';
+  }
+
+  @Get('bbb')
+  @UseInterceptors(TimeInterceptor)
+  bbb(): string {
+    console.log('bbb...');
+    return 'bbb';
+  }
+
+  @Get('ccc')
+  ccc(@Query('num', ValidatePipe) num: number) {
+    return num + 1;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { LoginGuard } from './login.guard';
+// import { NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // app.use(function (req: Request, res: Response, next: NextFunction) {
+  //   console.log('before', req.url);
+  //   next();
+  //   console.log('after');
+  // });
+  // app.useGlobalGuards(new LoginGuard());
+
   await app.listen(3000);
+  console.log('Application is running on: http://localhost:3000');
 }
 bootstrap();
