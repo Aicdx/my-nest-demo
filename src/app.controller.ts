6d01dc3e129c96deb0da98a6ed01f698dd160b1a
xuanquan <xuanquan@kingsoft.com>
2024-04-23 10:07:28 +0800

    arg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AaaException } from './AaaException';
+import { AaaGuard } from './aaa.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  // @UseFilters(AaaFilter)
+  @UseGuards(AaaGuard)
   getHello(): string {
+    throw new AaaException('aaa', 'bbb');
     return this.appService.getHello();
   }
 }
